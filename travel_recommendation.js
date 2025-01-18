08665c15fae073fe3e56c45e76f2149fc28cca6e
KODONOU narcisse <narcissekodonou@mail.com>
2025-01-18 02:41:22 +0100

    finish

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -4,79 +4,113 @@ const recommendation = [];
 
 
 function resetForm() {
-    document.getElementById("name").value = "";
-    document.querySelector('input[name="gender"]:checked').checked = false;
-    document.getElementById("age").value = "";
-    document.getElementById("condition").value = "";
-  }
+  document.getElementById("name").value = "";
+  document.querySelector('input[name="gender"]:checked').checked = false;
+  document.getElementById("age").value = "";
+  document.getElementById("condition").value = "";
+}
 
 
 function searchRecommendation() {
-    const input = document.getElementById('conditionInput').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
+  const input = document.getElementById('conditionInput').value.toLowerCase();
+  const resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = '';
 
-    fetch('travel_recommendation_api.json')
-      .then(response => response.json())
-      .then(data => {
-        console.log(input);
+  fetch('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(input);
 
-        switch (input) {
-          case "country":
-            const countries = data.countries;
+      switch (input) {
+        case "country":
+          const countries = data.countries;
+          resultDiv.innerHTML += `<h2>Countries</h2>`;
 
-            console.log(countries);
+          countries.forEach((country) => {
+            country.cities.forEach((city) => {
 
-
-            resultDiv.innerHTML += `<h2>${input}</h2>`;
-
-            countries.forEach((country)=>{
-              console.log(countries.name);
-              country.cities.forEach((city)=>{
-                resultDiv.innerHTML += `<img src="${city.imageUrl}" alt="hjh">`;
-              });
-
+              resultDiv.innerHTML += `
+                  <div style="text-align: center; margin-bottom: 20px;">
+                      <img src="${city.imageUrl}" alt="${city.name}" style="width: 100%; height: auto; border-radius: 5px;" />
+                      <div style="background-color:white;text-align:left">
+                        <h3 style="color:black">${city.name}</h3>
+                        <p style="font-size: 16px; line-height: 1.5;color:black">${city.description}</p>
+                        <button style="font-weight: bold;width:100px;">Visit</button>
+                      </div>
+                  </div>
+              `;
 
             });
+          });
 
-            break;
+          break;
 
-          case "temple":
-            const temples = data.temples;
-            break;
+        case "temple":
+          const temples = data.temples;
+          resultDiv.innerHTML += `<h2>Temples</h2>`;
 
-          case "beach":
-            const beaches = data.beaches;
-            break;
-        
-          default:
-            resultDiv.innerHTML = "Not fund, type countries, temples or beaches";
-            break;
-        }
+          temples.forEach((temple) => {
 
-        /*
-        
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+              resultDiv.innerHTML += `
+                  <div style="text-align: center; margin-bottom: 20px;">
+                      <img src="${temple.imageUrl}" alt="${temple.name}" style="width: 100%; height: auto; border-radius: 5px;" />
+                      <div style="background-color:white;text-align:left">
+                        <h3 style="color:black">${temple.name}</h3>
+                        <p style="font-size: 16px; line-height: 1.5;color:black">${temple.description}</p>
+                        <button style="font-weight: bold;width:100px;">Visit</button>
+                      </div>
+                  </div>
+              `;
+          });
+          break;
+
+        case "beach":
+          const beaches = data.beaches;
+          resultDiv.innerHTML += `<h2>Beaches</h2>`;
+
+          beaches.forEach((beach) => {
+
+              resultDiv.innerHTML += `
+                  <div style="text-align: center; margin-bottom: 20px;">
+                      <img src="${beach.imageUrl}" alt="${beach.name}" style="width: 100%; height: auto; border-radius: 5px;" />
+                      <div style="background-color:white;text-align:left">
+                        <h3 style="color:black">${beach.name}</h3>
+                        <p style="font-size: 16px; line-height: 1.5;color:black">${beach.description}</p>
+                        <button style="font-weight: bold;width:100px;">Visit</button>
+                      </div>
+                  </div>
+              `;
+          });
+          break;
+
+        default:
+          resultDiv.innerHTML = "Not fund, type country, temple or beach";
+          break;
+      }
+
+      /*
+      
+      const condition = data.conditions.find(item => item.name.toLowerCase() === input);
 
 
-        if (condition) {
-          const symptoms = condition.symptoms.join(', ');
-          const prevention = condition.prevention.join(', ');
-          const treatment = condition.treatment;
+      if (condition) {
+        const symptoms = condition.symptoms.join(', ');
+        const prevention = condition.prevention.join(', ');
+        const treatment = condition.treatment;
 
-          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+        resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+        resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
 
-          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
-        } else {
-          resultDiv.innerHTML = 'Condition not found.';
-        } */
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        resultDiv.innerHTML = 'An error occurred while fetching data.';
-      });
-  }
-    btnSearch.addEventListener('click', searchRecommendation);
+        resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+        resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+        resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+      } else {
+        resultDiv.innerHTML = 'Condition not found.';
+      } */
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      resultDiv.innerHTML = 'An error occurred while fetching data.';
+    });
+}
+btnSearch.addEventListener('click', searchRecommendation);
